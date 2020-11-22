683ff2e488d61d8195d8eaa51d571aa992b0b187
matthewogtong <ogtongm@gmail.com>
2020-11-22 15:56:05 -0500

    commit tree b82066805490e06f699cb88da71b6eb55ba4fc71

--- app/controllers/ducks_controller.rb
+++ app/controllers/ducks_controller.rb
@@ -4,4 +4,45 @@ class DucksController < ApplicationController
     @ducks = Duck.all
   end
 
+  def show
+    @duck = Duck.find(params[:id])
+  end
+
+  def new
+    @duck = Duck.new
+  end
+
+  def create
+    duck = Duck.create(duck_params)
+
+    if duck.valid?
+      redirect_to duck_path(duck)
+    else
+      flash[:my_errors] = duck.errors.full_messages
+      redirect_to new_duck_path
+    end
+  end
+
+  def edit
+    @duck = Duck.find(params[:id])
+  end
+
+  def update
+    duck = Duck.find(params[:id])
+    duck.update(duck_params)
+
+    if duck.valid?
+      redirect_to duck_path(duck)
+    else
+      flash[:my_errors] = duck.errors.full_messages
+      redirect_to edit_duck_path(duck)
+    end
+  end
+
+  private
+
+  def duck_params
+    params.require(:duck).permit(:name, :description, :student_id)
+  end
+
 end
--- app/controllers/students_controller.rb
+++ app/controllers/students_controller.rb
@@ -4,4 +4,45 @@ class StudentsController < ApplicationController
     @students = Student.all
   end
 
+  def show
+    @student = Student.find(params[:id])
+  end
+
+  def new
+    @student = Student.new
+  end
+
+  def create
+    student = Student.create(student_params)
+
+    if student.valid?
+      redirect_to student_path(student)
+    else
+      flash[:my_errors] = student.errors.full_messages
+      redirect_to new_student_path
+    end
+  end
+
+  def edit
+    @student = Student.find(params[:id])
+  end
+
+  def update
+    student = Student.find(params[:id])
+    student.update(student_params)
+
+    if student.valid?
+      redirect_to student_path(student)
+    else
+      flash[:my_errors] = student.errors.full_messages
+      redirect_to edit_student_path(student)
+    end
+  end
+
+  private
+
+  def student_params
+    params.require(:student).permit(:name, :mod)
+  end
+
 end
